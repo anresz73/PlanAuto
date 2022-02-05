52a122baeb9a6346e5d2b70e83775709723da106
anresz73 <andresreisz@gmail.com>
2022-02-04 22:47:51 -0300

    constants.py added

---
+++ PlanAuto/constants.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Created on Fri Feb  4 17:35:09 2022
+#
+# Arbitrador de Planes vs Capitalización
+#
+# https://github.com/anresz73/PlanAuto
+#
+
+"""
+Constantes para el seteo 
+"""
+
+from pandas import read_excel
+
+# Constantes
+
+VALOR_AUTO = 2400000.
+VALOR_CAPITALIZAR = 1200000.
+#CASH_FLOW = #data['importe'].sum()
